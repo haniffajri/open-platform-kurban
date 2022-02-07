2d7a0d7a7901d363871e6f08553b67311b4d6a7f
dell <mrf.rizkifauzi@gmail.com>
2022-02-07 10:12:39 +0700

    add modify page vendor and page product of vendor

---
+++ wp-content/themes/bistro/dokan/store-header.php
@@ -0,0 +1,47 @@
+<?php
+$store_user               = dokan()->vendor->get( get_query_var( 'author' ) );
+$store_info               = $store_user->get_shop_info();
+$social_info              = $store_user->get_social_profiles();
+$store_tabs               = dokan_get_store_tabs( $store_user->get_id() );
+$social_fields            = dokan_get_social_profile_fields();
+
+$dokan_appearance         = get_option( 'dokan_appearance' );
+$profile_layout           = empty( $dokan_appearance['store_header_template'] ) ? 'default' : $dokan_appearance['store_header_template'];
+$store_address            = dokan_get_seller_short_address( $store_user->get_id(), false );
+
+$dokan_store_time_enabled = isset( $store_info['dokan_store_time_enabled'] ) ? $store_info['dokan_store_time_enabled'] : '';
+$store_open_notice        = isset( $store_info['dokan_store_open_notice'] ) && ! empty( $store_info['dokan_store_open_notice'] ) ? $store_info['dokan_store_open_notice'] : __( 'Store Open', 'dokan-lite' );
+$store_closed_notice      = isset( $store_info['dokan_store_close_notice'] ) && ! empty( $store_info['dokan_store_close_notice'] ) ? $store_info['dokan_store_close_notice'] : __( 'Store Closed', 'dokan-lite' );
+$show_store_open_close    = dokan_get_option( 'store_open_close', 'dokan_appearance', 'on' );
+
+$general_settings         = get_option( 'dokan_general', [] );
+$banner_width             = dokan_get_vendor_store_banner_width();
+
+if ( ( 'default' === $profile_layout ) || ( 'layout2' === $profile_layout ) ) {
+    $profile_img_class = 'profile-img-circle';
+} else {
+    $profile_img_class = 'profile-img-square';
+}
+
+if ( 'layout3' === $profile_layout ) {
+    unset( $store_info['banner'] );
+
+    $no_banner_class      = ' profile-frame-no-banner';
+    $no_banner_class_tabs = ' dokan-store-tabs-no-banner';
+
+} else {
+    $no_banner_class      = '';
+    $no_banner_class_tabs = '';
+}
+
+?>
+<div class="dokan-profile-frame-wrapper">
+    <div class="profile-frame<?php echo esc_attr( $no_banner_class ); ?>">
+        <div class="profile-info-box profile-layout-<?php echo esc_attr( $profile_layout ); ?>">
+            <?php if ( ! empty( $store_user->get_shop_name() ) && 'default' !== $profile_layout ) { ?>
+                <h1 class="store-name"><?php echo esc_html( $store_user->get_shop_name() ); ?></h1>
+            <?php } ?>
+        </div> <!-- .profile-info-box -->
+    </div> <!-- .profile-frame -->
+
+</div>
