249ae78986cbedfb761656126ff295bd9bfd71cc
dell <mrf.rizkifauzi@gmail.com>
2022-02-13 11:20:43 +0700

    footer

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -17,15 +17,103 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="col-full">
 
+		<div class="footer-top-custom">
 			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' );
+				echo '<img src="'.site_url().'/wp-content/themes/bistro/assets/image/logo-footer.png" class="img-footer" />';
 			?>
+			<div class="sosmed-footer-wrap">
+				<div class="sosmed-footer-title">
+					Ikuti Kami di Sosial Media
+				</div>
+				<div class="sosmed-footer-logo">
+					<a href="<?php echo 'https://www.instagram.com/bantutetanggacom'; ?>" target="_blank" class="item-sosmed-footer">
+						<?php
+							echo '<img src="'.site_url().'/wp-content/themes/bistro/assets/image/Instagram.png" class="img-sosmed-footer" />';
+						?>
+					</a>
+
+					<a href="<?php echo 'https://www.facebook.com/bantutetanggadotcom'; ?>" target="_blank" class="item-sosmed-footer">
+						<?php
+							echo '<img src="'.site_url().'/wp-content/themes/bistro/assets/image/Facebook.png" class="img-sosmed-footer" />';
+						?>
+					</a>
+
+					<a href="<?php echo 'https://www.youtube.com/channel/UCbpgIZHxgQXHe_4DydVM8ow'; ?>" target="_blank" class="item-sosmed-footer">
+						<?php
+							echo '<img src="'.site_url().'/wp-content/themes/bistro/assets/image/Youtube.png" class="img-sosmed-footer" />';
+						?>
+					</a>
+
+					<a href="<?php echo 'https://vt.tiktok.com/ZSeG1hCXx/'; ?>" target="_blank" class="item-sosmed-footer">
+						<?php
+							echo '<img src="'.site_url().'/wp-content/themes/bistro/assets/image/Tiktok.png" class="img-sosmed-footer" />';
+						?>
+					</a>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="footer-content-custom">
+			<div class="footer-content-wrap">
+				<h2 class="footer-content-title">Bantu Tetangga</h2>
+				<p>Jl. Setra Dago Utama No. 27</p>
+				<p>Antapani Kulon, Kec. Antapani</p>
+				<p>Kota Bandung, Jawa Barat 40291</p>		
+			</div>
+
+			<div class="footer-content-wrap">
+				<h2 class="footer-content-title">Program</h2>
+				<ul>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Galang Dana</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Donasi</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Kurban</span></a>
+					</li>
+				</ul>	
+			</div>
+
+			<div class="footer-content-wrap">
+				<h2 class="footer-content-title">Lembaga</h2>
+				<ul>
+					<li>
+						<a href="<?php echo site_url().'/tentang-kurban'; ?>"><span>Tentang Kami</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Blog</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Syarat & Ketentuan</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Kebijakan Privasi</span></a>
+					</li>
+				</ul>	
+			</div>
+			
+			<div class="footer-content-wrap">
+				<h2 class="footer-content-title">Hubungi Kami</h2>
+				<ul>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Pesan WhatsApp</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Pesan Email</span></a>
+					</li>
+					<li>
+						<a href="<?php echo '/'; ?>"><span>Pusat Bantuan</span></a>
+					</li>
+				</ul>	
+			</div>
+
+		</div>
+		<div class="footer-end-custom">
+			© 2022 Bantu Tetangga
+		</div>
 
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
